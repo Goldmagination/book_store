96631c575f385bc3073df909bac8a72c47107f04
Goldmagination <62952539+Goldmagination@users.noreply.github.com>
2021-07-09 13:39:59 +0200

    start working with admin panel

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,12 +1,19 @@
 from django.http.response import Http404
 from django.shortcuts import get_object_or_404, render
+from django.db.models import Avg
 from .models import Book
 # Create your views here.
 
 
 def index(request):
-    books = Book.objects.all()
-    return render(request, "book_outlet/index.html", {"books": books})
+    books = Book.objects.all().order_by("-rating")
+    num_of_books=books.count()
+    avg_rating=books.aggregate(Avg("rating"))
+    return render(request, "book_outlet/index.html", {
+        "books": books,
+        "total_number_of_books": num_of_books,
+        "average_rating": avg_rating,
+        })
 
 
 def book_detail(request, slug):

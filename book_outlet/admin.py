21295b6669b03dcc20080991e2f65ff20eba05b3
Goldmagination <62952539+Goldmagination@users.noreply.github.com>
2021-07-11 19:34:27 +0200

    working with DB releations

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Author, Book, Address, Country
 
 # Register your models here.
 
@@ -8,7 +8,22 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("author", "rating")
-    list_display=("title","author")
+    list_display = ("title", "author")
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ("last_name",)
+    list_display = ("full_name", "address")
+
+class AddressAdmin(admin.ModelAdmin):
+    list_filter = ("city",)
+    list_display = ("street", "postcode", "city")
+
+class CountryDisplayed(admin.ModelAdmin):
+    list_display = ("name", "code",)
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Country, CountryDisplayed)
